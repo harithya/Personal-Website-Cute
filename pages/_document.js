9d6d772a016e1749bfd33b54c40410ba3e77c200
Harithya Wisesa <43434776+harithya@users.noreply.github.com>
2022-06-08 20:23:05 +0700

    remove meta

--- pages/_document.js
+++ pages/_document.js
@@ -5,15 +5,6 @@ export default function Document() {
         <Html>
             <Head>
                 <title>Hey there! I'm Della Rianty Febrian 👋</title>
-                <div>
-                    <meta name="title" content="Hey! I'm Della Rianty Febrian, a student and also a nutritionist 👋" />
-                    <meta name="description" content="I'm a student and also a nutritionist, anime and manga are things I like, but there are some things I hate including peanuts." />
-                    <meta name="keywords" content="Della Rianty Febria, della rianty, rianty, nutritionist, unsoed, student" />
-                    <meta name="robots" content="index, follow" />
-                    <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-                    <meta name="language" content="English" />
-                </div>
-
             </Head>
             <body>
                 <Main />
