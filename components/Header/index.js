1c43e7677b65788d7003d01a61c050a1a3fdca86
Harithya Wisesa <43434776+harithya@users.noreply.github.com>
2022-06-09 13:44:26 +0700

    fix blur effect

--- components/Header/index.js
+++ components/Header/index.js
@@ -22,7 +22,7 @@ const Header = () => {
 
     const [isOpen, setIsOpen] = useState(false)
     return (
-        <div className='sticky top-0 z-50 bg-[#f9f7f5]/80 backdrop-blur-sm  dark:bg-gray-700/80  right-0 left-0'>
+        <div className='sticky top-0 z-50 bg-[#f9f7f5]/60 backdrop-blur-sm  dark:bg-gray-700/80  right-0 left-0'>
             <Container className={`lg:py-8 py-6 flex  justify-between flex-row  items-center  ${isOpen && 'border-b-2 border-dashed dark:border-gray-600 border-peach-500'} lg:border-0`}>
                 <h5 className='font-medium text-lg'>Della Rianty Febrian</h5>
                 <div className='block'>
